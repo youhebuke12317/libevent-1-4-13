9b6e904a01e6331cc9cbf90dc8affe2afa960be0
youhebuke12317 <zldata@aliyun.com>
2017-04-18 20:15:30 +0800

    git.sh脚本尝试

---
+++ git.sh
@@ -0,0 +1,17 @@
+#########################################################################
+# File Name: git.sh
+# Author: reus
+# mail: dmarcoreus@1gmail.com
+# Created Time: 2017年04月18日 星期二 20时12分13秒
+#########################################################################
+#!/bin/bash
+
+git add .
+
+read -p "git commit 时写下说明: " discp
+
+git commit -m $discp
+
+
+
+
